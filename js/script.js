040c2391d1a53085dc7cd0176ae2dd137cc06dec
Ivan <ivan.burovkin@gmail.com>
2020-10-20 11:11:45 +0400

    modal

--- js/script.js
+++ js/script.js
@@ -94,4 +94,40 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline);
+
+    //Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close');
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            //modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';//убираем скролл приоткрытом модальном окне
+        });
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        //modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }//для оптимизации кода, чтобы не повторять код, который уже есть
+    
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });//если пользователь кликнул мимо модального окна
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });//закрытие модального окна при нажатии на кнопку Esc
 });
